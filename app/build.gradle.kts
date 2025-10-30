bc2fd0e9db2addcdf169b60ac964b3bd9e34531f
Daxton Brown <nugnuts.the.2nd@gmail.com>
2025-10-30 10:09:34 -0700

    Add main buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.learningconstraintlayout"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.learningconstraintlayout"
